983eb3c9313a114efd0e1ad6b81225e39aec2234
Harsh Kaushik <55058037+Harshkaushik14@users.noreply.github.com>
2022-02-10 17:17:54 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCT8ZjIFqXrhL0xndaPYx2qxY8fjIKgq6Q",
+    authDomain: "crud-ffe96.firebaseapp.com",
+    projectId: "crud-ffe96",
+    storageBucket: "crud-ffe96.appspot.com",
+    messagingSenderId: "1094483112056",
+    appId: "1:1094483112056:web:27c587f2e110061d1cfd8d"
+  };
+
+  const app = initializeApp(firebaseConfig);
